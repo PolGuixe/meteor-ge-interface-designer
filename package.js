b33c5b948a3f306203f9ff11c73b66205c71949e
PolGuixe <pol.guixe@gmail.com>
2016-01-12 01:15:09 +0100

    updated info

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Meteor wrapper for FIWARE GE Interface Designer',
   // URL to the Git repository containing the source code for this package.
-  //git: 'https://github.com/PolGuixe/meteor-ge-interface-designer',
+  git: 'https://github.com/PolGuixe/meteor-ge-interface-designer',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
